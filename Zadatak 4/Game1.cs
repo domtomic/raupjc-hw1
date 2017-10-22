4cb0088cee5f45e2d2ff766da3e57296d235e90d
DESKTOP-PQA6O0D\Domagoj <domagoj.tomic@fer.hr>
2017-10-22 23:33:21 +0200

    update

--- Zadatak 4/Game1.cs
+++ Zadatak 4/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Zadatak_3;
 
 namespace Zadatak_4
 {
@@ -14,9 +17,43 @@ namespace Zadatak_4
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferHeight = 900,
+                PreferredBackBufferWidth = 500
+            };
             Content.RootDirectory = "Content";
         }
+
+        /// <summary>
+        /// Bottom paddle object
+        /// </summary>
+        public Paddle PaddleBottom { get; private set; }
+        /// <summary>
+        /// Top paddle object
+        /// </summary>
+        public Paddle PaddleTop { get; private set; }
+        /// <summary>
+        /// Ball object
+        /// </summary>
+        public Ball Ball { get; private set; }
+        /// <summary>
+        /// Background image
+        /// </summary>
+        public Background Background { get; private set; }
+        /// <summary>
+        /// Sound when ball hits an obstacle.
+        /// SoundEffect is a type defined in Monogame framework
+        /// </summary>
+        public SoundEffect HitSound { get; private set; }
+        /// <summary>
+        /// Background music. Song is a type defined in Monogame framework
+        /// </summary>
+        public Song Music { get; private set; }
+        /// <summary>
+        /// Generic list that holds Sprites that should be drawn on screen
+        /// </summary>
+        private IGenericList<Sprite> SpritesForDrawList = new GenericList<Sprite>();
 
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
@@ -26,8 +63,28 @@ namespace Zadatak_4
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            // Screen bounds details . Use this information to set up game objects positions.
+            var screenBounds = GraphicsDevice.Viewport.Bounds;
+            PaddleBottom = new Paddle(GameConstants.PaddleDefaultWidth,
+                GameConstants.PaddleDefaulHeight, GameConstants.PaddleDefaulSpeed);
+            PaddleBottom.X = screenBounds.Width / 2f - PaddleBottom.Width / 2f;
+            PaddleBottom.Y = screenBounds.Bottom - PaddleBottom.Height;
+            PaddleTop = new Paddle(GameConstants.PaddleDefaultWidth,
+                GameConstants.PaddleDefaulHeight, GameConstants.PaddleDefaulSpeed);
+            PaddleTop.X = screenBounds.Width / 2f - PaddleBottom.Width / 2f;
+            PaddleTop.Y = 0;
+            Ball = new Ball(GameConstants.DefaultBallSize, GameConstants.DefaultInitialBallSpeed,
+                GameConstants.DefaultBallBumpSpeedIncreaseFactor)
+            {
+                X = screenBounds.Width / 2f,
+                Y = screenBounds.Height / 2f
+            };
+            Background = new Background(screenBounds.Width, screenBounds.Height);
+            // Add our game objects to the sprites that should be drawn collection.
+            SpritesForDrawList.Add(Background);
+            SpritesForDrawList.Add(PaddleBottom);
+            SpritesForDrawList.Add(PaddleTop);
+            SpritesForDrawList.Add(Ball);
             base.Initialize();
         }
 
@@ -37,10 +94,21 @@ namespace Zadatak_4
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
+            // Initialize new SpriteBatch object which will be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Set textures
+            Texture2D paddleTexture = Content.Load<Texture2D>("paddle");
+            PaddleBottom.Texture = paddleTexture;
+            PaddleTop.Texture = paddleTexture;
+            Ball.Texture = Content.Load<Texture2D>("ball");
+            Background.Texture = Content.Load<Texture2D>("background");
+            // Load sounds
+            // Start background music
+            HitSound = Content.Load<SoundEffect>("hit");
+            Music = Content.Load<Song>("music");
 
-            // TODO: use this.Content to load your game content here
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(Music);
         }
 
         /// <summary>
@@ -62,7 +130,40 @@ namespace Zadatak_4
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            var touchState = Keyboard.GetState();
+            if (touchState.IsKeyDown(Keys.Left))
+            {
+                PaddleBottom.X = PaddleBottom.X - (float)(PaddleBottom.Speed *
+                                                          gameTime.ElapsedGameTime.TotalMilliseconds);
+            }
+
+            if (touchState.IsKeyDown(Keys.Right))
+            {
+                PaddleBottom.X = PaddleBottom.X + (float)(PaddleBottom.Speed *
+                                                          gameTime.ElapsedGameTime.TotalMilliseconds);
+            }
+
+            if (touchState.IsKeyDown(Keys.A))
+            {
+                PaddleTop.X = PaddleTop.X - (float)(PaddleBottom.Speed *
+                                                          gameTime.ElapsedGameTime.TotalMilliseconds);
+            }
+
+            if (touchState.IsKeyDown(Keys.D))
+            {
+                PaddleTop.X = PaddleTop.X + (float)(PaddleBottom.Speed *
+                                                          gameTime.ElapsedGameTime.TotalMilliseconds);
+            }
+            var screenBounds = GraphicsDevice.Viewport.Bounds;
+            PaddleBottom.X = MathHelper.Clamp(PaddleBottom.X, screenBounds.Left, screenBounds.Right -
+                                                                           PaddleBottom.Width);
+            PaddleTop.X = MathHelper.Clamp(PaddleTop.X, screenBounds.Left, screenBounds.Right -
+                                                                                 PaddleTop.Width);
+
+            var ballPositionChange = Ball.Direction *
+                                     (float)(gameTime.ElapsedGameTime.TotalMilliseconds * Ball.Speed);
+            Ball.X += ballPositionChange.X;
+            Ball.Y += ballPositionChange.Y;
 
             base.Update(gameTime);
         }
@@ -73,11 +174,122 @@ namespace Zadatak_4
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Start drawing .
+            spriteBatch.Begin();
+            for (int i = 0; i < SpritesForDrawList.Count; i++)
+            {
+                SpritesForDrawList.GetElement(i).DrawSpriteOnScreen(spriteBatch);
+            }
+            // End drawing.
+            // Send all gathered details to the graphic card in one batch.
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
+    }
+
+    public abstract class Sprite
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        /// <summary>
+        /// Represents the texture of the Sprite on the screen.
+        /// Texture2D is a type defined in Monogame framework.
+        /// </summary>
+        public Texture2D Texture { get; set; }
+        protected Sprite(int width, int height, float x = 0, float y = 0)
+        {
+            X = x;
+            Y = y;
+            Height = height;
+            Width = width;
+        }
+        /// <summary>
+        /// Base draw method
+        /// </summary>
+        public virtual void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0,
+                Width, Height), Color.White);
+        }
+    }
+
+    /// <summary>
+    /// Game background representation
+    /// </summary>
+    public class Background : Sprite
+    {
+        public Background(int width, int height) : base(width, height)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Game ball object representation
+    /// </summary>
+    public class Ball : Sprite
+    {
+        /// <summary>
+        /// Defines current ball speed in time.
+        /// </summary>
+        public float Speed { get; set; }
+
+        public float BumpSpeedIncreaseFactor { get; set; }
+
+        /// <summary>
+        /// Defines ball direction.
+        /// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+        /// Using Vector2 to simplify game calculation . Potentially
+        /// dangerous because vector 2 can swallow other values as well.
+        /// OPTIONAL TODO : create your own , more suitable type
+        /// </summary>
+        public Vector2 Direction { get; set; }
+
+        public Ball(int size, float speed, float
+            defaultBallBumpSpeedIncreaseFactor) : base(size, size)
+        {
+            Speed = speed;
+            BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+            // Initial direction
+            Direction = new Vector2(1, 1);
+        }
+    }
+
+    /// <summary>
+    /// Represents player paddle.
+    /// </summary>
+    public class Paddle : Sprite
+    {
+        /// <summary>
+        /// Current paddle speed in time
+        /// </summary>
+        public float Speed { get; set; }
+        public Paddle(int width, int height, float initialSpeed) : base(width,
+            height)
+        {
+            Speed = initialSpeed;
+        }
+        /// <summary>
+        /// Overriding draw method . Masking paddle texture with black color.
+        /// </summary>
+        public override void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0,
+                Width, Height), Color.GhostWhite);
+        }
+    }
+
+    public class GameConstants
+    {
+        public const float PaddleDefaulSpeed = 0.9f;
+        public const int PaddleDefaultWidth = 200;
+        public const int PaddleDefaulHeight = 20;
+        public const float DefaultInitialBallSpeed = 0.4f;
+        public const float DefaultBallBumpSpeedIncreaseFactor = 1.05f;
+        public const int DefaultBallSize = 40;
     }
 }
